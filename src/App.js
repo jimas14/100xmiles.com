1e0eab6eb2ff7366fe3f8762babbfd0404f5d2da
Jimmy Schleicher <jimas14@gmail.com>
2022-03-10 22:49:47 -0500

    Update page layout; Switch npm for yarn

--- src/App.js
+++ src/App.js
@@ -4,37 +4,32 @@ import background from './img/background.mp4'
 
 function App() {
     return (
-        <div className='App'>
-            <video autoPlay muted loop id='background'>
+        <div className='app'>
+            <video id='background' autoPlay muted loop>
                 <source src={background} type='video/mp4'></source>
             </video>
-            <div className='container'>
-                {/* fit logo in screen */}
-                <img className='logo' src={logo} alt='100 Miles logo'></img>
-                <div className='nav'>
-                    <ul>
-                        {/* 
-                            actually make links navigate
-                            get blurb from grace
-                            fix font size 
-                        */}
-                        <li><a href='/'>Home</a></li>
-                        <li><a href='/about'>About</a></li>
-                        <li><a href='https://100miles.bigcartel.com/' target='_blank' rel='noreferrer'>Shop</a></li>
-                        <li><a href='/contact'>Contact</a></li>
-                    </ul>
-                </div>
-                <div className='player'>
-                    <div className='video-container'>
-                        {/* fit player in screen */}
-                        <iframe title='playlist' src='https://www.youtube.com/embed/videoseries?list=PLpD0fqTBFRaouHuGWR09h7-fX_syp4e8o' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>
+
+            <img id='logo' src={logo} alt='100 Miles logo'></img>
+
+            <div className='nav'>
+                <div className='container'>
+                    <div className='row'>
+                        <a className='col-md' href='/'>Home</a>
+                        <div className='col-md'></div>
+                        <a className='col-md' href='/about'>About</a>
+                        <div className='col-md'></div>
+                        <a className='col-md' href='https://100miles.bigcartel.com/' target='_blank' rel='noreferrer'>Shop</a>
+                        <div className='col-md'></div>
+                        <a className='col-md' href='/contact'>Contact</a>
                     </div>
                 </div>
-                <div className='socials'>
-                    <div className='box'>
-                    {/* add socials  */}
-                    </div>
-                </div>
+            </div>
+
+            <div className='socials'>
+            </div>
+
+            <div className='playlist'>
+                <iframe id='playlist' title='YouTube' src='https://www.youtube.com/embed/videoseries?list=PLpD0fqTBFRaouHuGWR09h7-fX_syp4e8o' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>
             </div>
         </div>
     );
